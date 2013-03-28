088058aeb60b2ea480f0bd6c5c52bca4da2e276d
Jason Wong <jason@arbutus>
2013-03-28 01:21:54 -0700

    Comment ratings disabled by default.  Registered with mezzanine settings

--- core/defaults.py
+++ core/defaults.py
@@ -9,3 +9,11 @@ register_setting(
     editable=True,
     default="/static/roca/header.jpg",
 )
+
+register_setting(
+	name="COMMENTS_USE_RATINGS",
+    label=_("Rate comments"),
+    description=_("Set TRUE to enable comment ratings.  False otherwise."),
+    editable=False,
+    default=False,
+)
